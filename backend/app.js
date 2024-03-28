507e27d88e7f5034966e87dfdf267283218d6a3f
Lorys <lorys.aveneau@gmail.com>
2024-03-28 17:45:15 +0100

    Add commands + testing

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/modal', (req, res) => {
+  console.log('GET /modal')
+  res.status(200).json({ title: 'Hello Modal!' })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- mobile-frontend/app/modal.tsx
+++ mobile-frontend/app/modal.tsx
@@ -3,11 +3,24 @@ import { Platform, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import React, { useEffect, useState } from 'react';
 
 export default function ModalScreen() {
+  const [modalTitle, setModalTitle] = useState('')
+  const backendUrl = process.env.EXPO_PUBLIC_BACKEND_URL
+
+  const getModalTitle = async () => {
+    const response = await fetch(backendUrl + '/modal')
+    return response.json().then(data => data.title)
+  }
+
+  useEffect(() => {
+    getModalTitle().then(setModalTitle)
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>{modalTitle}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/modal.tsx" />
 
